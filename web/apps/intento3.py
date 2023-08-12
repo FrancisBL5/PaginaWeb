c7110cb2a588739aadcb0bee24ace9d5a6655c6d
Jessica Zepeda <jessicazepbae@gmail.com>
2023-08-12 12:53:08 -0600

    Update intento3.py
    Se modifica el layout.
    Se elimina tabla de muestra del dataframe.
    Se agregan mensajes de verificación de carga de archivo correcto.
    Se modifica el diseño de las tablas de validación.

--- web/apps/intento3.py
+++ web/apps/intento3.py
@@ -17,10 +17,10 @@ df_sample_test = pd.DataFrame()
 res_bal = pd.DataFrame()
 res_pos = pd.DataFrame()"""
 
-def generate_table(df):
-    return html.Div([
-            dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
-        ]),
+#def generate_table(df):
+#    return html.Div([
+#            dash_table.DataTable(data=df.to_dict('records'), page_size=15, style_table={'overflowX': 'auto'})
+#        ]),
 
 def convertGraphToCY(G):
     """Función que transforma un grafo de NetworkX en un Cytoscape JSON format"""
@@ -60,97 +60,99 @@ def convertCYToGraph(cy):
     return G
 
 layout = [html.Div([
+    html.H2('Carga de Datos'),
     html.Br(),
-    html.Div(className='twelve columns', children='BD UNAM Completo',
-        style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
-    html.Br(),
-    html.Div([
-        dcc.Interval(id="progress-interval", n_intervals=0, interval=400, disabled=False),
-        dbc.Progress(id="progress", value = 0, label = "", animated = True)
-    ]),
-    html.Div([
-        html.P('Cargue el archivo csv que contiene los datos'),
-        dcc.Upload(id='upload-data', 
-    		children=html.Div([
-            	'Drag and Drop or ',
-            	html.A('Select Files')
-            ]), className = "twelve columns", style={
-            	'height': '60px',
-            	'lineHeight': '60px',
-            	'borderWidth': '1px',
-            	'borderStyle': 'dashed',
-            	'borderRadius': '5px',
-            	'textAlign': 'center',
-            	'margin': '10px'},
-    	),
-    ]),
-    html.Br(),
-    html.Div(id = 'html-table', className = "twelve columns"),
-    html.Br(),
-    html.Div([
-        html.P('Observe los dos grafos completos generados:')], className = 'twelve columns'),
-    dbc.Row([
-        dbc.Col([
-            html.P('Grafo completo', style = {
+    html.P('Cargue el archivo .csv que contiene los datos.'),
+    dcc.Upload(id='upload-data', children = html.Div([
+        'Drag and Drop or ',
+        html.A('Select Files')]), style={
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '3px',
+                'borderStyle': 'dashed',
+                'borderRadius': '10px',
                 'textAlign': 'center',
-                'fontSize': 20,
-                'color': '#349000'})
-        ], className = 'six columns'),
-        dbc.Col([
-            html.P('Sub grafo', style = {
-                'textAlign': 'center',
-                'fontSize': 20,
-                'color': '#349000'})
-        ], className = 'six columns')
-    ], className = 'twelve columns'),
+                'margin': '10px'},
+        ),
     html.Br(),
+    html.Div(dbc.Alert([
+        html.H5("Advertencia", className="alert-heading"),
+        html.P('Primero es necesario cargar un archivo con extensión .csv')], color="danger"), id='message'),
+    dcc.Interval(id="progress-interval", n_intervals=0, interval=400, disabled=False),
+    dbc.Progress(id="progress", value = 0, label = "", color='secondary', animated=True, style={"height": "30px"}),
+    ], style = {
+            'textAlign': 'justify',
+            'padding-top':'20px', 
+            'padding-left':'40px', 
+            'padding-right':'40px', 
+            'padding-bottom':'40px'}),
+
     html.Div([
-        html.P('Aplicando los algoritmos de Machine Learning se obtiene')], className = 'twelve columns'),
-    dbc.Row([
-        dbc.Col([
-            html.P('Scores balanceados', style = {
-                'textAlign': 'center',
-                'fontSize': 20,
-                'color': '#349000'}),
-            html.Div(id = 'res_bal'),
-        ], className = 'six columns'),
-        dbc.Col([
-            html.P('Scores positivos', style = {
-                'textAlign': 'center',
-                'fontSize': 20,
-                'color': '#349000'}),
-            html.Div(id = 'res_pos'),
-        ], className = 'six columns')
-    ], className = 'twelve columns'),
-    #dcc.Store(id='output-data-upload')
-])
-]
+        html.H2('Generación de Grafos'),
+        html.Br(),
+        dbc.Row([
+            dbc.Col([
+                html.H4('Grafo Completo', style={'textAlign': 'center'}),
+                ], width = 6),
+            dbc.Col([
+                html.H4('Subgrafo', style={'textAlign': 'center'}),
+                ], width = 6)
+            ]),
+        ], style = {
+            'textAlign': 'justify',
+            'padding-top':'20px', 
+            'padding-left':'40px', 
+            'padding-right':'40px', 
+            'padding-bottom':'40px'}),
+
+    html.Div([
+        html.H2('Validación de Modelos'),
+        html.P('Las métricas que arrojan los modelos creados son:'),
+        html.H4('Scores balanceados', style={'textAlign': 'center'}),
+        html.Div(id = 'res_bal'),
+        html.Br(),
+        html.H4('Scores positivos', style={'textAlign': 'center'}),
+        html.Div(id = 'res_pos'),
+        #dcc.Store(id='output-data-upload')
+        ], style = {
+            'textAlign': 'justify',
+            'padding-top':'20px', 
+            'padding-left':'40px', 
+            'padding-right':'40px', 
+            'padding-bottom':'40px'})
+    ]
 
 #____________ Callbacks __________
 #____________ Recibir el archivo inicial
 @app.callback(Output('output-data-upload', 'data'),
+              Output('message', 'children'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
               prevent_initial_call = True)
 def upload_datos(list_of_contents, filename):
     if list_of_contents is not None:
         try:
-            content_type, content_string = list_of_contents.split(',')
-            decoded = base64.b64decode(content_string)
-            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-            return df.to_json(date_format='iso', orient='split')
+            if '.csv' in filename:
+                content_type, content_string = list_of_contents.split(',')
+                decoded = base64.b64decode(content_string)
+                df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            else:
+                return [], dbc.Alert([
+                            html.H5("Advertencia", className="alert-heading"),
+                            html.P('El archivo cargado no tiene extensión .csv'),
+                            html.P('Inténtalo de nuevo')], color="danger"),
         except Exception as e:
-            return html.Div([
-                'There was an error processing this file.'
-            ])
+            print(e)
+            return [],html.Div(dbc.Alert("Algo raro pasa aquí", color="danger")),
+        return df.to_json(date_format='iso', orient='split'), []
 
 #____________ Generar tabla del archivo inicial
-@app.callback(Output('html-table', 'children'),
-              Input('output-data-upload', 'data'),
-              prevent_initial_call = True)
-def generateTableHtml(jsonified_cleaned_data):
-    df = pd.read_json(jsonified_cleaned_data, orient='split')
-    return generate_table(df)
+#@app.callback(Output('html-table', 'children'),
+#              Input('output-data-upload', 'data'),
+#              prevent_initial_call = True)
+#def generateTableHtml(jsonified_cleaned_data):
+#    df = pd.read_json(jsonified_cleaned_data, orient='split')
+#    return generate_table(df)
 
 #____________ Obtener grafos
 @app.callback(Output('graphCompleto', 'data'),
@@ -208,18 +210,22 @@ def getMedidas(sample_train_json, sample_test_json):
     df_sample_train = pd.read_json(sample_train_json, orient='split')
     df_sample_test = pd.read_json(sample_test_json, orient='split')
     res_bal, res_pos = models.transformVariables(df_sample_train, df_sample_test)
-    return generate_table(res_bal), generate_table(res_pos)
+    res_bal = dbc.Table.from_dataframe(round(res_bal,3), striped=True, bordered=True, hover=True)
+    res_pos = dbc.Table.from_dataframe(round(res_pos,3), striped=True, bordered=True, hover=True)
+    return res_bal,res_pos
 
+#__________ Actualizar progreso de la barra
 @app.callback(
     [Output("progress", "value"), Output("progress", "label"), Output("progress-interval", "disabled"), Output("progress", "animated")],
     Input("progress-interval", "n_intervals"),
+    State('message','children'),
     State('upload-data', 'contents'),
     State('output-data-upload', 'data'),
     State('graphCompleto', 'data'),
     State('sample_train', 'data'),
     State('res_bal', 'children'))
-def update_progress(n, dato_cargado, dato_html, grafo, sample, respuesta):
-    if dato_cargado is None:
+def update_progress(n, msg, dato_cargado, dato_html, grafo, sample, respuesta):
+    if dato_cargado is None or msg != []:
         return 0, "", False, False
     if dato_html is None:
         return 15, "Procesando archivo", False, True
@@ -231,6 +237,3 @@ def update_progress(n, dato_cargado, dato_html, grafo, sample, respuesta):
         return 60, "Compilando modelos", False, True
     else:
         return 100, "Todo listo", True, False
-
-if __name__ == '__main__':
-    app.run(debug=True)
